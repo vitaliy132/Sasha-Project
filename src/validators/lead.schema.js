5d8127e0fcf74b0fb6a7124b3851291e241c220d
vitaliy132 <vitaliymed060@gmail.com>
2026-02-12 14:43:07 +0000

    1

--- src/validators/lead.schema.js
+++ src/validators/lead.schema.js
@@ -4,8 +4,8 @@ const Joi = require("joi");
 module.exports = Joi.object({
   first_name: Joi.string().min(2).required(),
   last_name: Joi.string().min(2).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().min(6).required(),
+  email: Joi.string().email(),
+  phone: Joi.string().min(6),
   interest: Joi.string().allow(""),
   notes: Joi.string().allow(""),
   platform: Joi.string(),
